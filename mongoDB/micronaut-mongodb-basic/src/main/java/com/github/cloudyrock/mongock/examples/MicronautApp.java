98c79ac4d27b31d976cf0d4d9b4abc8363638147
Dhivesh Sadhwani <dhivesh.sadhwani@gmail.com>
2021-07-21 07:38:13 +0100

    Scaffolding project

---
+++ mongoDB/micronaut-mongodb-basic/src/main/java/com/github/cloudyrock/mongock/examples/MicronautApp.java
@@ -0,0 +1,9 @@
+package com.github.cloudyrock.mongock.examples;
+
+import io.micronaut.runtime.Micronaut;
+
+public class MicronautApp {
+    public static void main(String[] args) {
+        Micronaut.run(MicronautApp.class, args);
+    }
+}
